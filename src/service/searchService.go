e7c225f6fc02a2b776ec07756a85ae4641f2ef7e
ruxiliang <ruxiliang@outlook.com>
2021-10-18 00:18:17 +0800

    getController.go中除了K线图以外都写完了

--- src/service/searchService.go
+++ src/service/searchService.go
@@ -1,20 +1,24 @@
 package service
 
-func SearchStock(stockName string)(string,string,string){
+import "github.com/gin-gonic/gin"
+
+func SearchStock(stockName string) (string, string, string) {
 	//TODO call python function
-	return stockName,"",""
+	return stockName, "", ""
 }
 
 //TODO call dao function finish the job
-func SearchUserHoldings()  {
-
+func SearchUserHoldings(userId string) (string, []gin.H) {
+	// TODO
+	return userId, nil
 }
 
-func SearchUserInformations(){
-	
+func SearchUserInformations(userId string) (string, string, float64, float64) {
+	// TODO
+	return userId, "", 0.0, 0.0
 }
 
 //查询K线图
-func SearchK()  {
-	
+func SearchK() {
+
 }
